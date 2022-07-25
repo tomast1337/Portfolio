b2a3111e348e785146e472addef90ffe9a1846d2
tomast1337 <vycasnicolas@gmail.com>
2022-07-25 14:20:49 -0300

    Melhorias na pagina de resumo

--- src/pages/ResumoPage.tsx
+++ src/pages/ResumoPage.tsx
@@ -15,7 +15,13 @@ const ResumoPage = () => {
                 Sou programador há 8 anos e estudo formalmente desde 2016. Estudei TI no ensino médio técnico da FAETEC com foco em desenvolvimento de software Java, linguagem na qual tenho mais prática e experiência. Atualmente estou cursando Ciência da Computação no CEFET/RJ.
             </p>
             <p>
-                Meu objetivo é muda da área do meu estágio atual em ciência de dados no TCU, para um estágio ou emprego desenvolvedor de software que era meu objetivo inicial a 8 anos.
+                Meu objetivo atual é mudar da área do meu estágio atual em ciência de dados no TCU, para um estágio ou emprego desenvolvedor de software que era meu objetivo inicial a 8 anos.
+            </p>
+            <p>
+                Estou disposto a trabalhar em qualquer região do Brasil, ser for remoto, e presencial na cidade do Rio de Janeiro, meu tempo para universidade é inegociável, mas estou sempre disponível quando não estou estudando.
+            </p>
+            <p>
+                A cima a um botão "menu" onde se pode acessar outras paginas do meu portfólio, que tal começar pelos meus <a href="/Projetos">projetos</a>?
             </p>
         </div>
     )
