5d6338ac6faddab2660e5237224785bab95abaff
tomast1337 <vycasnicolas@gmail.com>
2022-08-13 00:09:05 -0300

    Varias melhorias

--- src/pages/ProjetosPage.tsx
+++ src/pages/ProjetosPage.tsx
@@ -32,19 +32,25 @@ const ProjetosPage = () => {
         {
             nome: "More Sponges mod",
             descricao: "Mod para Minecraft em java que adiciona mais esponjas ao jogo.",
-            imagem: "./src/imgs/moresponges.png",
+            imagem: "/imgs/moresponges.png",
             link: "https://github.com/tomast1337/More-Sponges-Mod"
         },
         {
             nome: "Pizzaria On",
-            descricao: "Projeto fullstack com ReactJs+Redux e Express que implementa um site de delivery de pizza customizadas.",
-            imagem: "./src/imgs/pizzaon.jpg",
+            descricao: "Projeto fullstack com ReactJs+Redux, Express e MongoDB que implementa um site de delivery de pizza customizadas.",
+            imagem: "/imgs/pizzaon.jpg",
             link: "https://github.com/dsm-cefet-rj/ti-2022-1-grupo_8"
+        },
+        {
+            nome: "Pizzaria Byte",
+            descricao: "Projeto fullstack remake da Pizzaria On, um site de delivery de pizza customizadas, em typescript com varias melhorias com ReactJs+Redux, Express e MongoDB.",
+            imagem: "/imgs/pizzaByte.webp",
+            link: "https://github.com/tomast1337/Pizzaria-Byte"
         },
         {
             nome: "Bot Rio Vagas",
             descricao: "WebScrapper feito com Python e Selenium que busca vagas e envia currículos no site do Rio Vagas.",
-            imagem: "./src/imgs/python+selenium.webp",
+            imagem: "/imgs/python+selenium.webp",
             link: "https://github.com/tomast1337/Rio-Vagas-Bot"
         },
         {
@@ -56,15 +62,29 @@ const ProjetosPage = () => {
         {
             nome: "Minecraft Visualização Estrutura de Dados",
             descricao: "Plugin spigot em java que gera visualizações de estrutura de dados do jogo.",
-            imagem: "./src/imgs/Minecraft Visualização Estrutura de Dados.png",
+            imagem: "/imgs/Minecraft Visualização Estrutura de Dados.png",
             link: "https://github.com/tomast1337/Minecraft-Visualizacao-Estrutura-de-Dados"
+        },
+        {
+            descricao: "TCC do meu técnico em informática da FAETEC de 2018. Controle de uma loja de roupas, feito com java, java swing e MySQL.",
+            nome: "Controle de loja de Roupas",
+            imagem: "https://github.com/tomast1337/exercism/blob/main/java/2015-2018/projeto%20vanessa%20e%20nicolas/class%20der.png?raw=true",
+            link: "https://github.com/tomast1337/exercism/tree/main/java/2015-2018/projeto%20vanessa%20e%20nicolas"
         },
         {
             nome: "Este projeto",
             descricao: "Site pessoal feito em React e TypeScript.",
-            imagem: "./src/imgs/Este projeto.png",
+            imagem: "/imgs/Este projeto.png",
             link: "https://github.com/tomast1337/Portf-lio"
         }
+        /*
+        {
+            nome: "",
+            descricao: "",
+            imagem: "",
+            link: ""
+        },
+        */
     ]
     React.useEffect(() => {
         document.title = "Projetos - Nicolas Vycas Nery";
