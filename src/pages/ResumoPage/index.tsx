228a2c53d81711704370e818773dfdb531dd4b7a
tomast1337 <vycasnicolas@gmail.com>
2022-09-17 21:13:40 -0300

    Melhorias so Resumo e experiencias

--- src/pages/ResumoPage/index.tsx
+++ src/pages/ResumoPage/index.tsx
@@ -13,13 +13,19 @@ const ResumoPage = () => {
             <h1>Resumo</h1>
             <img src="/imgs/eu.jpeg" alt="Eu" />
             <p>
-                Sou programador há 8 anos e estudo formalmente desde 2016. Estudei TI no ensino médio técnico da FAETEC com foco em desenvolvimento de software Java, linguagem na qual tenho mais prática e experiência. Atualmente estou cursando Ciência da Computação no CEFET/RJ.
+                Sou Nicolas, e programador há 8 anos e estudo formalmente desde 2016. Estudei TI no ensino médio técnico da FAETEC com foco em desenvolvimento de software Java, linguagem na qual tenho mais prática e experiência. Atualmente estou cursando Ciência da Computação no CEFET/RJ.
+            </p>
+            <p>
+                Tenho interesse em desenvolvimento fullstack, para min código é arte e meu principal interesse des dos 14 anos de idade. Não existe stack ou negocio desinteressante, o que importa é a solução que você vai entregar. Sou entusiasta de tecnologias FOSS e de código aberto, e usuário de Arch Linux desde 2019. 
             </p>
             <p>
                 Meu objetivo atual é mudar da área do meu estágio atual em ciência de dados no TCU, para um estágio ou emprego desenvolvedor de software que era meu objetivo inicial a 8 anos.
             </p>
             <p>
-                Estou disposto a trabalhar em qualquer região do Brasil, ser for remoto, e presencial na cidade do Rio de Janeiro, claro se não atrapalhar meu tempo para a universidade e estudo.
+                Atualmente tenho interesse emprego e, ou estagio, aqui no Rio de Janeiro, mas se for remoto posso trabalhar para qualquer lugar.
+            </p>
+            <p>
+                Meu tempo na faculdade é negociável e pode se ajustar a posição, me formo no segundo semestre de 2024.
             </p>
             <p>
                 A cima a uma lista de links onde se pode acessar outras paginas do meu portfólio, que tal começar pelos meus <Link to="/Projetos">projetos</Link>?
