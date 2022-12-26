503bcffe46bf20563a6070507c69930272a6b923
tomast1337 <vycasnicolas@gmail.com>
2022-12-26 16:09:21 -0300

    Melhorias

--- src/pages/ResumoPage/index.tsx
+++ src/pages/ResumoPage/index.tsx
@@ -3,35 +3,50 @@ import { Link } from "react-router-dom";
 import ResumoStyle from "./ResumoPage.module.scss";
 
 const ResumoPage = () => {
+  React.useEffect(() => {
+    document.title = "Resumo - Nicolas Vycas Nery";
+  });
 
-    React.useEffect(() => {
-        document.title = "Resumo - Nicolas Vycas Nery";
-    });
-
-    return (
-        <div className={ResumoStyle.page}>
-            <h1>Resumo</h1>
-            <img src="/imgs/eu.jpeg" alt="Eu" />
-            <p>
-                Sou Nicolas, e programador há 8 anos e estudo formalmente desde 2016. Estudei TI no ensino médio técnico da FAETEC com foco em desenvolvimento de software Java, linguagem na qual tenho mais prática e experiência. Atualmente estou cursando Ciência da Computação no CEFET/RJ.
-            </p>
-            <p>
-                Tenho interesse em desenvolvimento fullstack, para min código é arte e meu principal interesse des dos 14 anos de idade. Não existe stack ou negocio desinteressante, o que importa é a solução que você vai entregar. Sou entusiasta de tecnologias FOSS e de código aberto, e usuário de Arch Linux desde 2019. 
-            </p>
-            <p>
-                Meu objetivo atual é mudar da área do meu estágio atual em ciência de dados no TCU, para um estágio ou emprego desenvolvedor de software que era meu objetivo inicial a 8 anos.
-            </p>
-            <p>
-                Atualmente tenho interesse em emprego e, ou estagio, aqui no Rio de Janeiro, mas se for remoto posso trabalhar para qualquer lugar.
-            </p>
-            <p>
-                Meu tempo na faculdade é negociável e pode se ajustar a posição, me formo no segundo semestre de 2024.
-            </p>
-            <p>
-                A cima a uma lista de links onde se pode acessar outras paginas do meu portfólio, que tal começar pelos meus <Link to="/Projetos">projetos</Link>?
-            </p>
-        </div>
-    )
-}
+  return (
+    <div className={ResumoStyle.page}>
+      <h1>Resumo</h1>
+      <img src="/imgs/eu.jpeg" alt="Eu" />
+      <p>
+        Sou Nicolas, sou programador há 8 anos, estudo formalmente
+        desenvolvimento de software desde 2016. Estudei TI no ensino médio
+        técnico da FAETEC com foco em desenvolvimento de software Java,
+        linguagem na qual tenho mais prática e experiência. Atualmente estou
+        cursando Ciência da Computação no CEFET/RJ.
+      </p>
+      <p>
+        Tenho interesse em desenvolvimento fullstack. Não existe stack ou
+        negocio desinteressante, o que importa é a solução que vou entregar para
+        o problema.
+      </p>
+      <p>
+        Sou entusiasta de tecnologias FOSS e de código aberto, e usuário de Arch
+        Linux desde 2019, e Linux des de 2016.
+      </p>
+      <p>
+        Atualmente sou estagiário no{" "}
+        <strong>Tribunal de contas da União</strong> na area de Cientista de
+        Dados, onde trabalho com Python, PowerBI, SQL e outras tecnologias. E
+        sou desenvolvedor fullstack meio período na{" "}
+        <a href="https://www.linkedin.com/company/betabit-dev/">BetaBit</a>,
+        onde trabalho com React, NestJS, NodeJS, Typescript, PostgreSQL, AWS e
+        outras tecnologias.
+      </p>
+      <p>
+        A cima a uma lista de links onde se pode acessar outras paginas do meu
+        portfólio, que tal começar pelos meus{" "}
+        <Link to="/Projetos">projetos</Link>?
+      </p>
+      <p>
+        Caso queira entrar em contato comigo, minha preferencia e que seja pelo{" "}
+        <a href="https://www.linkedin.com/in/nicolas-vycas/">LinkedIn</a>.
+      </p>
+    </div>
+  );
+};
 
 export default ResumoPage;
--- src/pages/ShoutOuts/index.tsx
+++ src/pages/ShoutOuts/index.tsx
@@ -2,69 +2,87 @@ import * as React from "react";
 import ShoutOutsStyle from "./ShoutOuts.module.scss";
 
 interface shoutOut {
-    nome: string;
-    link: string;
-    mensagem: string;
-    foto: string;
+  nome: string;
+  link: string;
+  mensagem: string;
+  foto: string;
 }
 
 const ShoutOutsPage = () => {
+  const shoutOuts: shoutOut[] = [
+    {
+      nome: "Davi Campanaro",
+      link: "https://github.com/DaveTheCamper",
+      mensagem: "",
+      foto: "https://avatars.githubusercontent.com/u/44680026?v=4",
+    },
+    {
+      nome: "Cristhiane da Cruz",
+      link: "https://github.com/barroscruzc",
+      mensagem: "",
+      foto: "https://avatars.githubusercontent.com/u/93226440?v=4",
+    },
+    {
+      nome: "Bernardo Costa",
+      link: "https://github.com/Bentroen",
+      mensagem: "",
+      foto: "https://avatars.githubusercontent.com/u/29354120?v=4",
+    },
+    {
+      nome: "Diego de Paula",
+      link: "https://github.com/DiegoPaula",
+      mensagem: "",
+      foto: "https://avatars.githubusercontent.com/u/77380810?v=4",
+    },
+    {
+      nome: "Felipe Nieto",
+      link: "https://github.com/NietoCurcio",
+      mensagem: "",
+      foto: "https://avatars.githubusercontent.com/u/51931239?v=4",
+    },
+    {
+      nome: "Nadson Nascimento",
+      link: "https://www.linkedin.com/in/nadson-cardoso/",
+      mensagem: "",
+      foto: "https://media.licdn.com/dms/image/C4E03AQFq0VFTp7srDA/profile-displayphoto-shrink_200_200/0/1655175845655?e=1677715200&v=beta&t=c3_hPaBOsorLNSWTKXqRpCRGW01BclO60aUynuE7sfw",
+    },
+    {
+      nome: "Thiago André",
+      link: "https://www.linkedin.com/in/thiago1590/",
+      mensagem: "",
+      foto: "https://media.licdn.com/dms/image/C4D03AQFZ3NqnBjv6mw/profile-displayphoto-shrink_800_800/0/1609030711153?e=1677715200&v=beta&t=mOk0FhbEcF0WxL9X7fdW_xMnce2XG5149dDs0AJwqnQ",
+    },
+  ];
 
-    const shoutOuts: shoutOut[] = [
-        {
-            nome: "Davi Campanaro",link: "https://github.com/DaveTheCamper",
-            mensagem: "",foto: "https://avatars.githubusercontent.com/u/44680026?v=4"
-        },
-        {
-            nome: "Cristhiane da Cruz",link: "https://github.com/barroscruzc",
-            mensagem: "",foto: "https://avatars.githubusercontent.com/u/93226440?v=4"
-        },
-        {
-            nome: "Bernardo Costa",link: "https://github.com/Bentroen",
-            mensagem: "",foto: "https://avatars.githubusercontent.com/u/29354120?v=4"
-        },
-        {
-            nome: "Diego de Paula",link: "https://github.com/DiegoPaula",
-            mensagem: "",foto: "https://avatars.githubusercontent.com/u/77380810?v=4"
-        },
-        {
-            nome: "Felipe Nieto",link: "https://github.com/NietoCurcio",
-            mensagem: "",foto: "https://avatars.githubusercontent.com/u/51931239?v=4"
-        },
-        {
-            nome: "Marcus Oliveira",link: "https://github.com/mvgoliveira",
-            mensagem: "",foto: "https://avatars.githubusercontent.com/u/53785487?v=4"
-        }
-    ];
+  React.useEffect(() => {
+    document.title = "ShoutOuts - Nicolas Vycas Nery";
+  });
 
-    React.useEffect(() => {
-        document.title = "ShoutOuts - Nicolas Vycas Nery";
-    });
-
-    return (
-        <div className={ShoutOutsStyle.page}>
-            <h1>Shout-outs</h1>
-            <p>
-                Pagina dedicada a programadores e profissionais que conheci durante os meus anos de estudo e aprecio o trabalho deles.
-            </p>
-            <div className={ShoutOutsStyle.shoutOuts}>
-                {
-                    shoutOuts.sort(() => Math.random() - 0.5).map((shoutOut, index) => {
-                        return (
-                            <div className={ShoutOutsStyle.shoutOut} key={index}>
-                                <img src={shoutOut.foto} alt={shoutOut.nome} />
-                                <div className={ShoutOutsStyle.shoutOutInfo}>
-                                    <h2>{shoutOut.nome}</h2>
-                                    {shoutOut.mensagem && <p>{shoutOut.mensagem}</p>}
-                                    <a href={shoutOut.link}>GitHub Profile</a>
-                                </div>
-                            </div>
-                        );
-                    })
-                }
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={ShoutOutsStyle.page}>
+      <h1>Shout-outs</h1>
+      <p>
+        Pagina dedicada a programadores e profissionais que conheci durante os
+        meus anos de estudo e aprecio o trabalho deles.
+      </p>
+      <div className={ShoutOutsStyle.shoutOuts}>
+        {shoutOuts
+          .sort(() => Math.random() - 0.5)
+          .map((shoutOut, index) => {
+            return (
+              <div className={ShoutOutsStyle.shoutOut} key={index}>
+                <img src={shoutOut.foto} alt={shoutOut.nome} />
+                <div className={ShoutOutsStyle.shoutOutInfo}>
+                  <h2>{shoutOut.nome}</h2>
+                  {shoutOut.mensagem && <p>{shoutOut.mensagem}</p>}
+                  <a href={shoutOut.link}>Link</a>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
 
 export default ShoutOutsPage;
