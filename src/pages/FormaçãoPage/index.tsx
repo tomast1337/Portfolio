248dbb93572bdc4cfcf98fa4008472ba74d0b3e7
tomast1337 <vycasnicolas@gmail.com>
2022-12-27 18:16:30 -0300

    Changes in Formacao page

--- src/pages/FormaçãoPage/index.tsx
+++ src/pages/FormaçãoPage/index.tsx
@@ -1,26 +1,44 @@
 import * as React from "react";
-import FormaçãoStyle from "./FormaçãoPage.module.scss";
+import {
+  Header,
+  Page,
+  SubHeader,
+  UnOrderedList,
+} from "../../components/Commom";
 
 const FormaçãoPage = () => {
-    React.useEffect(() => {
-        document.title = "Formação - Nicolas Vycas Nery";
-    });
-    return (
-        <div className={FormaçãoStyle.page}>
-            <h1>Formação</h1>
-            <h2>Ensino Superior</h2>
-            <p className={FormaçãoStyle.bold}>Em andamento</p>
-            <p>(Bacharelado em Ciência da Computação) no CEFET/RJ</p>
-            <p>Centro Federal de Educação Tecnológica</p>
-            <p className={FormaçãoStyle.bold}>(2020 – 2023)</p>
+  React.useEffect(() => {
+    document.title = "Formação - Nicolas Vycas Nery";
+  });
+  return (
+    <Page>
+      <Header>Formação</Header>
+      <SubHeader float="left">Ensino Superior</SubHeader>
+      <UnOrderedList>
+        <li>
+          <strong>Em andamento</strong>
+        </li>
+        <li>(Bacharelado em Ciência da Computação) no CEFET/RJ</li>
+        <li>Centro Federal de Educação Tecnológica</li>
+        <li>
+          <strong>(2020 – 2023)</strong>
+        </li>
+      </UnOrderedList>
 
-            <h2>Ensino Médio Técnico</h2>
-            <p className={FormaçãoStyle.bold}>Completo</p>
-            <p>(ensino técnico em informática) na FAETEC</p>
-            <p>Escola Técnica Estatual República</p>
-            <p className={FormaçãoStyle.bold}> (2016 – 2018)</p>
-        </div>
-    );
-}
+      <SubHeader float="left">Ensino Médio Técnico</SubHeader>
+      <UnOrderedList>
+        <li>
+          <strong>Completo</strong>
+        </li>
+        <li>(ensino técnico em informática) na FAETEC</li>
+        <li>Escola Técnica Estatual República</li>
+        <li>
+          {" "}
+          <strong>(2016 – 2018)</strong>
+        </li>
+      </UnOrderedList>
+    </Page>
+  );
+};
 
 export default FormaçãoPage;
