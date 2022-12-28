b4ff7076b11417316d74aafbf4da77a7c74a5170
tomast1337 <vycasnicolas@gmail.com>
2022-12-28 00:48:58 -0300

    New backgrounds

--- src/components/PageBody.tsx
+++ src/components/PageBody.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
 import Container from "../components/Container";
 import Navbar from "../components/Navbar";
+import { ThemeContext } from "styled-components";
 
 const PageBody = (props: { children: React.ReactNode }) => {
+  const { isDarkMode } = React.useContext(ThemeContext) as AppThemeContextType;
   React.useEffect(() => {
-    document.body.style.backgroundImage = "url('/imgs/pageBgtest.jpg')";
-  }, []);
+    if (isDarkMode) {
+      document.body.style.backgroundImage = "url('/imgs/WhiteBG.jpg')";
+    } else {
+      document.body.style.backgroundImage = "url('/imgs/BlackBG.jpg')";
+    }
+  }, [isDarkMode]);
+  document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundAttachment = "fixed";
   return (
     <>
       <Container>
